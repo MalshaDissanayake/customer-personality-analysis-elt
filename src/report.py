1f8174f7f2f35c6f33e3394ae214befb2391facc
MalshaDissanayake <dmkmd333@gmail.com>
2024-04-03 11:28:25 +0530

    Modify report.py

--- src/report.py
+++ src/report.py
@@ -7,8 +7,8 @@ def get_database_credentials():
     db_name = os.environ.get("DB_NAME")
     db_user = os.environ.get("DB_USER")
     db_password = os.environ.get("DB_PASSWORD")
-    db_host = os.environ.get("DB_HOST", "localhost")
-    db_port = os.environ.get("DB_PORT", "5432")
+    db_host = os.environ.get("localhost")
+    db_port = os.environ.get("5432")
     return db_name, db_user, db_password, db_host, db_port
 
 # Function to connect to the PostgreSQL database
